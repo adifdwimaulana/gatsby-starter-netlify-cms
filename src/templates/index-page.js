87951635d57eac302af2c757271b5a7305351218
adifdwimaulana <adifdwimaulana@gmail.com>
2020-06-20 21:25:49 +0700

    Hero section

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -33,11 +33,11 @@ export const IndexPageTemplate = ({
               <p>
                 Passionate and self-motivated <span>Software Engineer</span> and I have 2+ years of experience in related fields of tech.
                 Currently, I work as a <span>Freelancer</span> and have done 20+ projects about Web, Mobile Apps, and IoT devices.
-                Also, I like to contribute to the <span>Open Source Community </span> to share my knowledge to developers around the world
+                Also, I like to contribute to the <span>Open Source Community </span> to share my knowledge to developers around the world.
               </p>
               <div className="button-wrapper">
-                <button className="hire">Hire Me</button>
-                <button className="cv">Get CV</button>
+                <button className="hire"><a href="https://api.whatsapp.com/send?phone=6282236056425&text=Hai%20Adif,%20I've%20seen%20your%20portfolio&source=&data=">Hire Me</a></button>
+                <button className="cv"><a href="https://www.linkedin.com/in/adifdwimaulana/">Get CV</a></button>
               </div>
             </div>
             <div className="column">
